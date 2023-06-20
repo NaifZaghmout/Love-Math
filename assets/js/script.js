8a0a1ee9b87c5130c6a7e19b2836f1be82057315
NaifZaghmout zaghmout <naif-zaghmout@hotmail.com>
2023-06-20 14:24:39 +0000

    add js 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,10 +8,12 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame("gameType");
             }
         });
     }
+
+    runGame("addition");
 });
 
 /**
@@ -19,9 +21,17 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the users answer has been prcessed
  */
 
-function runGame() {
+function runGame(gameType) {
 let num1= Math.floor(Math.random() *25)+ 1 ;
 let num2= Math.floor(Math.random() *25)+ 1 ;
+
+if (gameType === "addition") {displayAdditionQuestion (num1 , num2);
+
+} else {
+    alert('unknown game type: ${gameType}');
+    throw 'unknown game type: ${gameType}. Aborting!';
+}
+
 }
 
 function checkAnswer() {
@@ -40,7 +50,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1 , operand2) {
+    document.getElementById('operand1').textContent= operand1;
+    document.getElementById('operand2').textContent= operand2;
+    document.getElementById('operator').textContent= "+";
 
 }
 
